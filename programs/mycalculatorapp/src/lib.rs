5f935a579f69ae8024c72df58d0f0d485d843d86
Devansh Purohit <devanshpurohit25@gmail.com>
2022-05-28 13:34:04 +0530

    Added functionality for other basic mathematical operations. Project Complete.

--- programs/mycalculatorapp/src/lib.rs
+++ programs/mycalculatorapp/src/lib.rs
@@ -13,7 +13,31 @@ pub mod mycalculatorapp {
         Ok(())
     }
 
-    // pub fn add(ctx: Contact<Addition>, num1: i64, num2: i64) -<
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) ->Result<()> {
+
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok(())
+    }
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) ->Result<()> {
+
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
+    }
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) ->Result<()> {
+
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        Ok(())
+    }
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) ->Result<()> {
+
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.remainder = num1 % num2;
+        calculator.result = num1 / num2;
+        Ok(())
+    }
 
 }
 
@@ -24,6 +48,30 @@ pub struct Create<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info>{
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info>{
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info>{
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Division<'info>{
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
 }
 
 #[account]
--- tests/mycalculatorapp.ts
+++ tests/mycalculatorapp.ts
@@ -1,22 +1,24 @@
+import { AnchorProvider } from "@project-serum/anchor"
+
 const assert = require('assert')
-const anchor = require('@project-serum/anchor');
-const { pathToFileURL } = require('url');
-const { isTypedArray } = require('util/types');
+const anchor = require('@project-serum/anchor')
+// const { pathToFileURL } = require('url');
+// const { isTypedArray } = require('util/types');
 const {SystemProgram} = anchor.web3
 
 describe('mycalculatorapp', ()=> {
 
     // abstraction of connection to solana network
-    const provider = anchor.Provider.local();
-    anchor.setProvider(provider);
+    const provider = anchor.AnchorProvider.local()
+    anchor.setProvider(provider)
 
     //generating credentials for a web3 Account
     const calculator = anchor.web3.Keypair.generate()
 
-    const program = anchor.workspace.Mycalculatordapp
+    const program = anchor.workspace.Mycalculatorapp
 
     it('Creates a calculator', async() => {
-        await program.rpc.create("Welcome to solana", {
+        await program.rpc.create("Welcome to Solana", {
             accounts: {
                 calculator: calculator.publicKey,
                 user: provider.wallet.publicKey,
@@ -29,5 +31,44 @@ describe('mycalculatorapp', ()=> {
         assert.ok(account.greeting === 'Welcome to Solana')
     })
 
+    it('Checks sum of Addition function', async()=> {
+        await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+            accounts:{
+                calculator: calculator.publicKey
+            }
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.ok(account.result == 5)
+    })
+    
+    it('Checks sum of Subtraction function', async()=> {
+        await program.rpc.subtract(new anchor.BN(2), new anchor.BN(3), {
+            accounts:{
+                calculator: calculator.publicKey
+            }
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.ok(account.result == -1)
+    })
 
+    it('Checks sum of Multiplication function', async()=> {
+        await program.rpc.multiply(new anchor.BN(2), new anchor.BN(3), {
+            accounts:{
+                calculator: calculator.publicKey
+            }
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.ok(account.result == 6)
+    })
+
+    it('Checks sum of Division function', async()=> {
+        await program.rpc.divide(new anchor.BN(2), new anchor.BN(3), {
+            accounts:{
+                calculator: calculator.publicKey
+            }
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.ok(account.result == 0)
+        assert.ok(account.remainder == 2)
+    })
 })
